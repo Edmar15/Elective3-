c42234992f21bccd0137a45854355e4bac98419f
Edmar15 <98209868+Edmar15@users.noreply.github.com>
2022-08-05 19:47:20 +0800

    Add files via upload

---
+++ exercise_2.m
@@ -0,0 +1,19 @@
+fruits = imread("fruits.png");
+
+imfinfo "fruits.png"
+%the data type of the picture is  integer uint8
+
+reduce_res= imresize(fruits,0.5);
+imwrite(reduce_res,"fruits2.png");
+
+output_res = imread("fruits2.png");
+
+imshow(output_res);
+
+imfinfo "fruits.png"
+imfinfo "fruits2.png"
+
+hsv_conversion = rgb2hsv(fruits);
+imwrite(hsv_conversion, "fruits3.png");
+hsv_img = imread("fruits3.png");
+figure(2),imshow(hsv_img);
